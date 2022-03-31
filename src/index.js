1924375591a6a037109adf0e49eae3f40dfb40ec
Ol-create <oluola96@gmail.com>
2022-03-31 17:30:33 -0600

    fix: remove linters err

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import _, { forEach } from 'lodash';
 import './style.css';
 
 // declear global variable
-  let todoTask = [
-    {"description":"First, wake-up", "completed": false , "index": 0},
-    {"description":"Second, do exercise", "completed": false , "index": 1},
-    {"description":"Third, say a prayer", "completed": false , "index": 2},
-    {"description":"Fourth, bath yourself", "completed": false , "index": 3},
-    { "description":"Fifth, wear a cloth", "completed": false , "index": 4 }
+const todoTask = [
+  { description: 'First, wake-up', completed: false, index: 0 },
+  { description: 'Second, do exercise', completed: false, index: 1 },
+  { description: 'Third, say a prayer', completed: false, index: 2 },
+  { description: 'Fourth, bath yourself', completed: false, index: 3 },
+  { description: 'Fifth, wear a cloth', completed: false, index: 4 },
 ];
 
 const divBox = document.createElement('div');
 const list = document.querySelector('#list');
 
-
-for(let i=0; i<todoTask.length; i++){
+for (let i = 0; i < todoTask.length; i += 1) {
   divBox.innerHTML += `
-      <li class="activity" completed = ${todoTask[i]['completed']} index = ${todoTask[i]['index']}>
+      <li class="activity" completed = ${todoTask[i].completed} index = ${todoTask[i].index}>
         <input type="checkbox" class="checkbox">
-        <p class= 'toDo' contenteditable="true">${todoTask[i]['description']}</p>
+        <p class= 'toDo' contenteditable="true">${todoTask[i].description}</p>
         <a class="move">
           <span class="material-icons-outlined">more_vert</span>
         </a>
@@ -27,5 +25,5 @@ for(let i=0; i<todoTask.length; i++){
         </a>
       </li>
     `;
-    list.appendChild(divBox);
-};
+  list.appendChild(divBox);
+}
